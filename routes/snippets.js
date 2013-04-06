6a50986baf080a9a57cd3f5d14f2e6c9fe28a39a
Jay Zehngebot <jay.zehngebot@gmail.com>
2013-04-06 12:42:37 -0400

    add candle image

---
+++ routes/snippets.js
@@ -0,0 +1,15 @@
+
+
+	//IMPORT DATA SNIPPET
+	
+				// for (i in allSkulls){
+				// 	currSkull = allSkulls[i];
+				
+				// 	currSkull.save(function(err){
+				// 		if(err){
+				// 			console.error('error on save');
+				// 		} else {
+				// 			console.log('skull candle count added to db');
+				// 		}
+				// 	});
+				// };
